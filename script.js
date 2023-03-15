428d05bdb92226b470e28a87f79dc832bf49e47f
Georgeches <georgechesire83@gmail.com>
2023-03-15 17:45:44 +0300

    reponsive update

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-
 $(document).ready(function(){
+  if (screen.width < 600){
+      $('.navbar-list').hide()
+      $('.details').hide()
+      $('.cross').hide()
+  }
+  else{
     $('.view').hide()
-
     $(".service").hover(function(){
       $(this).find('.view').toggle()
       $(this).find('.details').hide()
@@ -19,10 +23,22 @@ $(document).ready(function(){
           $(this).find('.view').hide()
           $(this).find('.details').toggle()
       });
+  }
+
+  $('.bars').click(function(){
+      $('.navbar-list').toggle()
+      $('.cross').toggle()
+      $('.bars').hide()
+  }); 
+
+  $('.cross').click(function(){
+    $('.navbar-list').hide()
+    $('.cross').hide()
+    $('.bars').toggle()
   });
 
+});
 function emailSubmit() {
-    senderName = document.getElementById('name').value
-    alert('Thank you ' + senderName + ' for reaching out.')
+  senderName = document.getElementById('name').value
+  alert('Thank you ' + senderName + ' for reaching out.')
 }
-
